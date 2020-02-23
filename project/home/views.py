83b7f0203c21f9e9444fee7c3ce603718c270e94
Story DeWeese <sdeweese@scu.edu>
2020-02-22 16:55:05 -0800

    Initial HTML files

--- project/home/views.py
+++ project/home/views.py
@@ -4,13 +4,17 @@ from django.http import HttpResponse
 
 # Create your views here.
 def homepage(request):
-    return HttpResponse("Hello, world. This is our homepage.")
+    #return HttpResponse("Hello, world. This is our homepage.")
+    return render(request, "index.html")
 
 def quiz(request):
-    return HttpResponse("Quiz page")
+    #return HttpResponse("Quiz page")
+    return render(request, "quiz.html")
 
 def results(request):
-    return HttpResponse("Results page")
+    #return HttpResponse("Results page")
+    return render(request, "results.html")
 
 def info(request):
-    return HttpResponse("Info page")
+    #return HttpResponse("Info page")
+    return render(request, "info.html")
