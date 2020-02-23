1bd5f4d9336c94899b5fe848c0ce052c53801cc9
agigliotti24 <agigliotti24@gmail.com>
2020-02-22 16:09:37 -0800

    Skeletons for more pages

--- project/home/urls.py
+++ project/home/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
+    path('quiz', views.quiz, name='quiz'),
+    path('results', views.results, name='results'),
+    path('info', views.info, name='info'),
 ]
--- project/home/views.py
+++ project/home/views.py
@@ -5,3 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 def homepage(request):
     return HttpResponse("Hello, world. This is our homepage.")
+
+def quiz(request):
+    return HttpResponse("Quiz page")
+
+def results(request):
+    return HttpResponse("Results page")
+
+def info(request):
+    return HttpResponse("Info page")
