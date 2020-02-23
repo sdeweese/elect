1dc27e8af30321a67962d89dff31499cbb0c8109
Story DeWeese <sdeweese@scu.edu>
2020-02-22 18:33:56 -0800

    trial code

--- project/home/views.py
+++ project/home/views.py
@@ -11,10 +11,13 @@ def quiz(request):
     #return HttpResponse("Quiz page")
     return render(request, "quiz.html")
 
-def results(request):
+def candidates(request):
     #return HttpResponse("Results page")
-    return render(request, "results.html")
+    return render(request, "candidates.html")
 
-def info(request):
+def vote(request):
     #return HttpResponse("Info page")
-    return render(request, "info.html")
+    return render(request, "vote.html")
+
+def results(request):
+    return render(request, "results.html")
